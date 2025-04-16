5e409249477891172002145d382c7e20bda598a8
Andrew Balaban <andrewbal93@gmail.com>
2025-04-16 22:35:43 +0300

    Push

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('#nav')
+const navBtnImg = document.querySelector('#nav-btn-img')
+const navBtn = document.querySelector('#nav-btn')
+const burgerBg = document.querySelector('#burger-bg')
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = './img/nav-close.svg'
+  } else {
+    navBtnImg.src = './img/nav-open.svg'
+  }
+}
+
+AOS.init({
+  once: true,
+  disable: 'mobile',
+})
